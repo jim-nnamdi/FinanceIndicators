47875258336c2e5a9c09a8e44e7797b4a5e386e8
jim-nnamdi <jimsamuel50@gmail.com>
2022-12-12 13:22:59 +0100

    variable argument template class defs

---
+++ varag/handlevar2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstdarg>
+
+using namespace std;
+
+template<class T> 
+class Accounts{
+    public:
+        T accountsCreated(int num, ...)
+};
+
+template<class T>
+T Accounts<T>::accountsCreated(int num, ...){
+    va_list user_accounts;
+    T i;
+    double sum  = 0.0;
+    for(i = 0; i < num; i++){
+        sum += va_arg(user_accounts, int);
+    }
+    va_end(user_accounts);
+    avg_created_accounts = sum/num;
+    return avg_created_accounts;
+}
+
+int main(){
+    Accounts<double> a;
+    double xx = a.accountsCreated(4,5,6,7,8,8,9);
+    cout << "average of created accounts" <<  xx << endl;
+}
